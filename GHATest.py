2b50bc7c50e7e5176602b459a03da882d8d7f324
waltwilo <144280698+waltwilo@users.noreply.github.com>
2024-07-12 16:06:02 -0600

    Update GHATest.py

--- GHATest.py
+++ GHATest.py
@@ -6,8 +6,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-a')    
 parser.add_argument('-p')  
-parser.add_argument('-l')
-parser.add_argument('-i')  
+parser.add_argument('-l') 
 
 subprocess.run("curl", "-s", "http://whatismyip.akamai.com/")
 
@@ -15,6 +14,6 @@ args = parser.parse_args()
 print(args)
 
 
-test = subprocess.run(["ssh", "-i", args.i, "-l", args.l, "-p", args.p, args.a, "build", "test", "-s"]) 
+test = subprocess.run(["ssh", "-i", "id_rsa", "-l", args.l, "-p", args.p, args.a, "build", "test", "-s"]) 
 
 # print("TEST\n" + test)
