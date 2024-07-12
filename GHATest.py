47371922ceb95285c48a6c32273598d5b504cf08
waltwilo <144280698+waltwilo@users.noreply.github.com>
2024-07-12 13:59:35 -0600

    Update GHATest.py

--- GHATest.py
+++ GHATest.py
@@ -1,3 +1,16 @@
-import os
+import argparse
+import subprocess
 
-print(os.environ)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-a')    
+parser.add_argument('-p')  
+parser.add_argument('-l')
+parser.add_argument('-i')  
+
+args = parser.parse_args()
+print(args)
+
+test = subprocess.run(["ssh", "-i", args.i, "-l", args.l, "-p", args.p, args.a, "build", "test", "-s"]) 
+
+# print("TEST\n" + test)
